f2ff56b74f093d453a30784addbd06402e694a90
Andrew Catton <>
2018-02-12 21:19:10 +0000

    Added the ability to add a missing definition

---
+++ actions/maybe-add-definition/function.js
@@ -0,0 +1,13 @@
+function(assetId, shouldContinue, ellipsis) {
+  const EllipsisApi = require('ellipsis-api');
+const ellipsisApi = new EllipsisApi(ellipsis);
+
+if (shouldContinue) {
+  ellipsisApi.run({
+    actionName: "add-definition",
+    args: [ { name: "asset", value: assetId } ]
+  }).then(ellipsis.noResponse);
+} else {
+  ellipsis.success("OK, maybe later then :thumbsup:")
+}
+}
