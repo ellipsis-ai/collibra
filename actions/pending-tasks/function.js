0d7da8fc350b98a8e7dc5e19d29367388aea8dc2
Andrew Catton <>
2018-02-22 01:15:24 +0000

    Factored out saved logins and use that to get username in tasks output

--- actions/pending-tasks/function.js
+++ actions/pending-tasks/function.js
@@ -1,18 +1,25 @@
 function(ellipsis) {
   const moment = require('moment');
+const getLogin = require('saved-login');
 const CollibraApi = require('collibra-api');
 const api = CollibraApi(ellipsis);
 
-api.listWorkflowTasks().then(tasks => {
-  ellipsis.success(tasks.results.map(ea => {
-    const link = api.linkFor("asset", ea.businessItem.id) + "#task-id=" + ea.id;
-    return {
-      id: ea.id,
-      title: ea.title,
-      description: ea.description,
-      dueDate: moment(ea.dueDate).fromNow(),
-      link: link
-    };
-  }));
+api.listWorkflowTasks().then(res => {
+  getLogin(ellipsis).then(login => {
+    const tasks = res.results.map(ea => {
+      const link = api.linkFor("asset", ea.businessItem.id) + "#task-id=" + ea.id;
+      return {
+        id: ea.id,
+        title: ea.title,
+        description: ea.description,
+        dueDate: moment(ea.dueDate).fromNow(),
+        link: link
+      };
+    });
+    ellipsis.success({
+      tasks: tasks,
+      username: login.username
+    });
+  });
 });
 }
