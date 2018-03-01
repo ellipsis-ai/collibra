306764a6d147620b8d29292048b727f9aaa266fd
Andrew Catton <>
2018-03-01 21:49:27 +0000

    Use the workflow logic to have an ongoing conversation with the bot

--- actions/complete-review-task/function.js
+++ actions/complete-review-task/function.js
@@ -1,18 +1,10 @@
 function(task, comment, action, ellipsis) {
-  const CollibraApi = require('collibra-api');
-const api = CollibraApi(ellipsis);
+  const workflowTask = require('workflow-task')(ellipsis);
 
 const formProperties = {
   comment: comment,
-  approve: (action.toLowerCase() == "approve"),
-  reject: (action.toLowerCase() == "reject")
+  approve: (action.id.toLowerCase() == "approve"),
+  reject: (action.id.toLowerCase() == "reject")
 };
-api.completeTask(task.id, formProperties).then(res => {
-  if (res.success) {
-    ellipsis.success(":white_check_mark: OK, completed\n\n" + JSON.stringify(res.success));
-  } else {
-    ellipsis.success(`Sorry, I couldn't complete this task.\n\n${res.error}`)
-  }
-  ellipsis.success(JSON.stringify(res));
-})
+workflowTask(task, formProperties);
 }
