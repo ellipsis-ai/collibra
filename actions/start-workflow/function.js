9f8e424c8b2a073f87ef822b1c41d0fb8ee21730
Andrew Catton <>
2018-03-08 19:10:09 +0000

    Couple of UX tweaks

--- actions/start-workflow/function.js
+++ actions/start-workflow/function.js
@@ -8,24 +8,23 @@ const workflowHelpers = require('workflow-helpers')(ellipsis);
 collibra.startAssetApprovalWorkflowFor(asset.id).then(res => {
   if (res.success) {
     const message = `OK, I started an asset approval workflow for \`${asset.label}\``;
-    ellipsisApi.say({ message: message }).then(res => {
-      collibra.nextTaskForAsset(asset.id).then(nextTask => {
-        if (nextTask) {
+    collibra.nextTaskForAsset(asset.id).then(nextTask => {
+      if (nextTask) {
+        ellipsisApi.say({ message: message }).then(res => {
           ellipsisApi.say({ message: "Here is the first task:" }).then(res => {
             ellipsisApi.run({
               actionName: "complete-task",
               args: [ { name: "task", value: nextTask.id }]
             }).then(ellipsis.noResponse);
           });
-        } else {
-          ellipsis.success();
-        }
-      });
+        });
+      } else {
+        ellipsis.success(message);
+      }
     });
   } else {
     const msg = `Sorry, I couldn't start this workflow yet.\n\n${res.error}`;
     workflowHelpers.addResponsibilityIfNec(res, msg, asset.id);
   }
-  
 });
 }
