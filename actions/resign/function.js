28a7ea5b659997abd19c8e67c16339dad72c7f9a
Andrew Catton <>
2018-04-06 19:40:58 +0000

    More text improvements

--- actions/resign/function.js
+++ actions/resign/function.js
@@ -5,7 +5,10 @@ const deleteSessionTokenFor = require('session-token').deleteSessionTokenFor;
 deleteSavedLoginForCurrentUser(ellipsis).then(res => {
   const deleted = res.data && res.data.deleteWhereLogin[0] ? res.data && res.data.deleteWhereLogin[0] : undefined;
   (deleted ? deleteSessionTokenFor(deleted.username, ellipsis) : Promise.resolve()).then(res => {
-    const msg = (deleted && deleted.username) ? `OK, you are no longer \`${deleted.username}\`.` : `Resignation accepted!`;
+    const msg = 
+      (deleted && deleted.username) ? 
+        `OK, you are no longer \`${deleted.username}\`. You are now the default Collibra user` : 
+        `You were already the default Collibra user.`;
     ellipsis.success(msg);
   });
 });
