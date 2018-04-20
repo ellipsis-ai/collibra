3983b6023e744fe6679157b0864dc544eb0a3fb4
Andrew Catton <>
2018-04-20 15:45:58 +0000

    Fixed an issue Pieter noticed: make the tasks in the dropdown more distinguishable by putting asset name first

--- data_types/WorkflowTask/function.js
+++ data_types/WorkflowTask/function.js
@@ -4,18 +4,25 @@ const CollibraApi = require('collibra-api');
 const api = CollibraApi(ellipsis);
 
 api.listWorkflowTasks().then(results => {
-  ellipsis.success(results.map(ea => {
-    const dueString = moment(ea.dueDate).fromNow();
-    const label = `${ea.description} (due ${dueString})`
-    return {
-      id: ea.id,
-      label: label,
-      type: ea.type,
-      description: ea.description,
-      assetId: ea.businessItem.id,
-      formRequired: ea.formRequired.toString(),
-      key: ea.key
-    };
-  }));
+  Promise.all(results.map(ea => {
+    return api.findAsset(ea.businessItem.id).then(asset => {
+      return { task: ea, asset: asset };
+    });
+  })).then(resultsWithAssets => {
+    ellipsis.success(resultsWithAssets.map(ea => {
+      const task = ea.task;
+      const dueString = moment(task.dueDate).fromNow();
+      const label = `${ea.asset.name}: ${task.description} (due ${dueString})`
+      return {
+        id: task.id,
+        label: label,
+        type: task.type,
+        description: task.description,
+        assetId: task.businessItem.id,
+        formRequired: task.formRequired.toString(),
+        key: task.key
+      };
+    }));
+  });
 });
 }
