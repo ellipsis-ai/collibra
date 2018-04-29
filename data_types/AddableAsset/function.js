dd7f49498db837eedbc08214ed664db9d8d3aafd
Andrew Catton <>
2018-04-29 16:24:25 +0000

    Bunch of tweaks heading into the Collibra conference demos: new triggers, better asset data type behavior

--- data_types/AddableAsset/function.js
+++ data_types/AddableAsset/function.js
@@ -1,11 +1,23 @@
 function(searchQuery, ellipsis) {
   const assetsMatching = require('asset-helpers').assetsMatching;
-
-const extraOptions = [
-  { id: "ellipsis-add-new", label: `Add a new asset named "${searchQuery}"…`, searchQuery: searchQuery }
-];
-
 assetsMatching(searchQuery, ellipsis).then( matches => {
-  ellipsis.success(matches.concat(extraOptions));
+  ellipsis.success(matches.concat(extraOptionsFor(matches)));
 });
+
+function extraOptionsFor(matches) {
+  const exactMatch = matches.find(ea => ea.label.toLowerCase().trim() === searchQuery.toLowerCase().trim());
+  if (exactMatch) {
+    return [];
+  } else {
+    return [
+      {
+        id: "ellipsis-add-new",
+        label: matches.length > 0 ?
+          `Add a new asset named “${searchQuery}”…` :
+          `No matches found — add a new one named “${searchQuery}”…`,
+        searchQuery: searchQuery
+      }
+    ];
+  }
+}
 }
