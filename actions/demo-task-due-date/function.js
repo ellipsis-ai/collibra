97af7e47b270c0680f2ef0387c999bcbf3979c17
Luke Andrews <luke@ellipsis.ai>
2019-06-12 15:12:28 +0000

    Make Koen's demo triggers work with out mention but only if triggered by Koen

--- actions/demo-task-due-date/function.js
+++ actions/demo-task-due-date/function.js
@@ -1,5 +1,10 @@
 function(ellipsis) {
+  const DemoHelpers = require('demo-helpers');
+if (!DemoHelpers.botMentionedOrIsKoen(ellipsis)) {
+  ellipsis.noResponse();
+} else {
   const moment = require('moment-timezone');
-const aDate = moment.tz(ellipsis.team.timeZone).add(3, 'days').format("MMM D, YYYY");
-ellipsis.success(`The due date is ${aDate}.`);
+  const aDate = moment.tz(ellipsis.team.timeZone).add(3, 'days').format("MMM D, YYYY");
+  ellipsis.success(`The due date is ${aDate}.`);
+}
 }
--- actions/new-request-waiting/function.js
+++ actions/new-request-waiting/function.js
@@ -1,33 +1,36 @@
 function(ellipsis) {
-  // Write a Node.js (8.10) function that calls ellipsis.success() with a result.
-// You can require any NPM package.
-const name = ellipsis.event.user.fullName || "friend";
-ellipsis.success(`
-Hello, ${name}. You have a new dataset registration request waiting.
+  const DemoHelpers = require('demo-helpers');
+if (!DemoHelpers.botMentionedOrIsKoen(ellipsis)) {
+  ellipsis.noResponse();
+} else {
+  const name = ellipsis.event.user.fullName || "friend";
+  ellipsis.success(`
+  Hello, ${name}. You have a new dataset registration request waiting.
 
-**[➡️ Go to task](https://dgc-koen-demo-dev.eu-west-1.lake.collibra.ai/tasks/my)**
-`, {
-  choices: [{
-    label: "Reject",
-    actionName: "demo-mock",
-    args: [{
-      name: "message",
-      value: "OK, the task has been rejected."
+  **[➡️ Go to task](https://dgc-koen-demo-dev.eu-west-1.lake.collibra.ai/tasks/my)**
+  `, {
+    choices: [{
+      label: "Reject",
+      actionName: "demo-mock",
+      args: [{
+        name: "message",
+        value: "OK, the task has been rejected."
+      }]
+    }, {
+      label: "Re-assign",
+      actionName: "demo-mock",
+      args: [{
+        name: "message",
+        value: "OK, this task has been re-assigned."
+      }]
+    }, {
+      label: "Discuss",
+      actionName: "demo-mock",
+      args: [{
+        name: "message",
+        value: "OK, I will open a discussion about this task."
+      }]
     }]
-  }, {
-    label: "Re-assign",
-    actionName: "demo-mock",
-    args: [{
-      name: "message",
-      value: "OK, this task has been re-assigned."
-    }]
-  }, {
-    label: "Discuss",
-    actionName: "demo-mock",
-    args: [{
-      name: "message",
-      value: "OK, I will open a discussion about this task."
-    }]
-  }]
-});
+  });
+}
 }
